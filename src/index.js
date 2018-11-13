22810a35039bf3f867905cf0fbdbc1ace9f3a191
Taras_Kovalev <taraswww777@mail.ru>
2018-11-13 17:42:52 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
+//
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.unregister();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {createStore} from 'redux';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function playlist(state = [], action) {
+	console.log('action: ', action);
+	switch (action.type) {
+		case 'ADD_TRACK':
+			return [
+				...state,
+				action.payload
+			];
+	}
+
+	return state;
+}
+
+const store = createStore(playlist);
+
+store.subscribe(() => {
+	console.log('store subscribe: ', store.getState());
+});
+
+store.dispatch({type: 'ADD_TRACK', payload: 'wewwe'});
+store.dispatch({type: 'ADD_TRACK', payload: 'wewwe3'});
